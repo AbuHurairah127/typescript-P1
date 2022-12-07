e93781cdcf2668f7d23f5c6e6436cd6d8c907cb0
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-12-07 20:46:34 +0500

    zia kahn repo added

---
+++ Clone of learn typescript with extra comments for better understanding/step05b_object_aliased/app.ts
@@ -0,0 +1,33 @@
+// anonymous
+
+let teacher: { name: string; exp: number } = {
+  name: "Zeeshan",
+  exp: 10,
+};
+
+// Aliased Object Type
+type Student = {
+  name: string;
+  age?: number; //Optional value can have or not
+};
+
+let student: Student = {
+  name: "Hira",
+  age: 30,
+};
+let abuhurairah: Student = {
+  name: "Abu Hurairah",
+};
+console.log(student["name"]);
+console.log(student.age);
+
+// Interfaces
+
+interface Manager {
+  name: string;
+  subordiates?: number; //Optional value can have or not
+}
+
+let storeManager: Manager = {
+  name: "Bilal",
+};
