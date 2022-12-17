3c88437870cceacf0c6fe836e2fa931f041c26db
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-12-17 12:24:59 +0500

    inheritance updated

--- Clone of learn typescript with extra comments for better understanding/step13ab_inheritanco_concepting/app.ts
+++ Clone of learn typescript with extra comments for better understanding/step13ab_inheritanco_concepting/app.ts
@@ -23,3 +23,9 @@ class Student extends Human {
 }
 let bassam: Human = new Student("Ch Bassam Tanvir", "Abu Hurairah");
 bassam.showFullName();
+
+// The data type (Class type) given to an object will be taken always
+// For instance, If we give a parent class type to an object
+// and store a child object as Animal is having Donkey or Snake in the previous file, it's true
+// But the opposite will not happen
+// Mean to say that we can store a parent object in the child typed obbject
